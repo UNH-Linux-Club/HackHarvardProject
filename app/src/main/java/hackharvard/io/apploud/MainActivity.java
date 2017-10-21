3d97a0bec8d140fa80feed5030988cdbb7f1c886
ep1040 <ep1040@wildcats.unh.edu>
2017-10-20 23:34:18 -0400

    Added Toast message

--- app/src/main/java/hackharvard/io/apploud/MainActivity.java
+++ app/src/main/java/hackharvard/io/apploud/MainActivity.java
@@ -1,7 +1,11 @@
 package hackharvard.io.apploud;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toastReceive(View v)
+    {
+        Context appContext = getApplicationContext();
+        CharSequence textOfToast = "Apploud";
+        int lengthOfToast = Toast.LENGTH_SHORT;
+
+        Toast.makeText(appContext, textOfToast, lengthOfToast).show();
+        Log.d("", "Apploud");
     }
 }
